4d1da6a5ba35724e2d669d9c20592a3559bc4c5d
amauri-castro <amauri.p.castro@gmail.com>
2023-12-04 15:22:44 -0300

    adicionado form de cadastro de produtos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,10 @@ import { BlueDirective } from './directives/blue.directive';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { HttpClientModule}  from '@angular/common/http';
 
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule} from "@angular/material/input";
+
 
 @NgModule({
   declarations: [
@@ -42,7 +46,11 @@ import { HttpClientModule}  from '@angular/common/http';
     MatCardModule,
     MatButtonModule,
     MatSnackBarModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -11,8 +11,8 @@ import { Product } from '../product.model';
 export class ProductCreateComponent implements OnInit {
 
   product: Product = {
-    name: 'Produto de teste',
-    price: 125.90
+    name: '',
+    price: 0.00
   } 
 
   constructor(private productService: ProductService,
