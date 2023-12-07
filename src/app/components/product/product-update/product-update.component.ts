037f3c196e22dd670abfe869db219308880d4a32
amauri-castro <amauri.p.castro@gmail.com>
2023-12-06 22:40:19 -0300

    implementado componente product update

--- src/app/components/product/product-update/product-update.component.ts
+++ src/app/components/product/product-update/product-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-update',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductUpdateComponent implements OnInit {
 
-  constructor() { }
+  product?: Product;
+
+  constructor(private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getProduct();
+  }
+
+  getProduct(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if(id)
+    this.productService.readById(id).subscribe(product => {
+      this.product = product;
+    })
+  }
+
+  updateProduct(): void {
+    if(this.product)
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMessage('Produto atualizado com sucesso');
+      this.router.navigate(['/products']);
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products']);
   }
 
 }
